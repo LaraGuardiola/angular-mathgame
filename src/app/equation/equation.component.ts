8157af6092c893ce3629cb94236d23b62c932f19
Sergio <webdev.slara@gmail.com>
2022-06-01 16:42:02 +0200

    refactored with rxjs solution for the avg time per solution

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { delay, filter } from 'rxjs/operators';
+import { delay, filter, scan } from 'rxjs/operators';
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -29,15 +29,16 @@ export class EquationComponent implements OnInit {
   get b() { return this.mathForm.value.b }
 
   ngOnInit(): void {
-    const startTime = new Date()
-    let numberSolved = 0 
-
-
     this.mathForm.statusChanges.pipe(
       filter(status => status === 'VALID'),
-      delay(200)
-    ).subscribe(() => {
-      numberSolved++
+      delay(200),
+      scan((acc) => {
+        return {
+          numberSolved: acc.numberSolved + 1,
+          startTime: acc.startTime
+        }
+      }, { numberSolved: 0, startTime: new Date()})
+    ).subscribe(({ numberSolved, startTime }) => {
       this.secondsPerSolution = (
         new Date().getTime() - startTime.getTime()
       ) / numberSolved / 1000
