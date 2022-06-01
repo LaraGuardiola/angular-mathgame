97bbbab9d72736c2dff0c4e766617963c68999e9
Sergio <webdev.slara@gmail.com>
2022-06-01 16:33:05 +0200

    solved average time per solution the angular way, ready for rxjs

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -9,6 +9,7 @@ import { MathValidators } from '../math-validators';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
+  secondsPerSolution: number = 0
   mathForm = new FormGroup(
     {
       a: new FormControl(this.randomNumber()),
@@ -28,10 +29,18 @@ export class EquationComponent implements OnInit {
   get b() { return this.mathForm.value.b }
 
   ngOnInit(): void {
+    const startTime = new Date()
+    let numberSolved = 0 
+
+
     this.mathForm.statusChanges.pipe(
       filter(status => status === 'VALID'),
       delay(200)
     ).subscribe(() => {
+      numberSolved++
+      this.secondsPerSolution = (
+        new Date().getTime() - startTime.getTime()
+      ) / numberSolved / 1000
       this.mathForm.setValue({
         a: this.randomNumber(),
         b: this.randomNumber(),
